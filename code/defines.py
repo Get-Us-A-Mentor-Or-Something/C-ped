a3cf56293a4de985ea232a264b51113c4768fa69
LudwigVonChesterfield <andreydell@yahoo.com>
2020-03-08 19:49:03 +0200

    Client representation on backend.
    - Client class represents the current connection with some client.
    - Client_info class represents the info that should be preserved between different clients of same *ownership*.

---
+++ code/defines.py
@@ -0,0 +1,9 @@
+SERVER_PERMISSION_NONE = 0
+
+SERVER_PERMISSION_BASIC = 1  # Basic permissions for users. Such as the help command.
+SERVER_PERMISSION_MOD = 2  # Ban, kick, save, load permissions for admins.
+
+SERVER_PERMISSION_ADMIN = 3
+SERVER_PERMISSION_ALL = 3
+
+USER_ID_ANONYMOUS = -1
